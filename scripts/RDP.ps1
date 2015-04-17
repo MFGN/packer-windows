390623b356e32efb7ee7e3ac95e204d5325bca06
MFGN <ignify@gmail.com>
2015-04-17 11:09:06 +0100

    Replacing .bat with .ps1

---
+++ scripts/RDP.ps1
@@ -0,0 +1,23 @@
+$RegistryKeys = @(
+    [pscustomobject]@{
+        'Name' = "fDenyTSConnections"
+        'Path' = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\"
+        'RegValue' = 0
+        'RegType' = 'DWord'
+    }
+)
+
+ForEach ($Item in $RegistryKeys)
+{
+    if ((Get-ItemProperty $Item.Path) -like "*$($Item.Name)*")
+    {
+        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+    else
+    {
+        New-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+}
+
+Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP -Profile Private,Domain -Enabled True -Action Allow
+Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-UDP -Profile Private,Domain -Enabled True -Action Allow
---
+++ scripts/no-auto-login.ps1
@@ -0,0 +1,20 @@
+$RegistryKeys = @(
+    [pscustomobject]@{
+        'Name' = "AutoAdminLogon"
+        'Path' = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\"
+        'RegValue' = 0
+        'RegType' = 'String'
+    }
+)
+
+ForEach ($Item in $RegistryKeys)
+{
+    if ((Get-ItemProperty $Item.Path) -like "*$($Item.Name)*")
+    {
+        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+    else
+    {
+        New-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+}
