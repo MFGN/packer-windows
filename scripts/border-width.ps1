1baa973ae1100c52988d2ef72eb1ce13ced15e57
MFGN <ignify@gmail.com>
2015-04-17 09:14:01 +0100

    Replacing .bat with .ps1

---
+++ scripts/border-width.ps1
@@ -0,0 +1,33 @@
+﻿if ((Get-PSDrive -PSProvider Registry).Root -notcontains 'HKEY_USERS')
+{
+    New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+}
+
+$Keys=@{
+    "BorderWidth" =  0
+    "PaddedBorderWidth" = 0
+}
+
+$HKCUpath = "HKCU:\Control Panel\Desktop\WindowMetrics\"
+$HKUpath = "HKU:\.DEFAULT\Control Panel\Desktop\WindowMetrics\"
+
+$Keys.GetEnumerator() | % {
+    #Change border width for current user
+    if ((Get-ItemProperty $HKCUpath) -like "*$($_.Name)*")
+    {
+        Set-ItemProperty -Path $HKCUpath -Name $_.Name -Value $_.Value -Type String -Force
+    }
+    else
+    {
+        New-ItemProperty -Path $HKCUpath -Name $_.Name -Value $_.Value -Type String -Force 
+    }
+    #Change border width for all users
+    if ((Get-ItemProperty $HKUpath) -like "*$($_.Name)*")
+    {
+        Set-ItemProperty -Path $HKUpath -Name $_.Name -Value $_.Value -Type String -Force
+    }
+    else
+    {
+        New-ItemProperty -Path $HKUpath -Name $_.Name -Value $_.Value -Type String -Force
+    }
+}
---
+++ scripts/registry-tweaks.ps1
@@ -0,0 +1,43 @@
+﻿$RegistryKeys = @(
+    [pscustomobject]@{
+        'Name' = "QuickEdit"
+        'Path' = "HKCU:\Console\"
+        'RegValue' = 1
+        'RegType' = 'DWord'
+    },
+    [pscustomobject]@{
+        'Name' = "HideFileExt"
+        'Path' = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\"
+        'RegValue' = 0
+        'RegType' = 'DWord'
+    },
+    [pscustomobject]@{
+        'Name' = "Start_ShowRun"
+        'Path' = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\"
+        'RegValue' = 1
+        'RegType' = 'DWord'
+    },
+    [pscustomobject]@{
+        'Name' = "StartMenuAdminTools"
+        'Path' = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\"
+        'RegValue' = 1
+        'RegType' = 'DWord'
+    }
+)
+
+if ((Get-PSDrive -PSProvider Registry).Root -notcontains 'HKEY_USERS')
+{
+    New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+}
+
+ForEach ($Item in $RegistryKeys)
+{
+    if ((Get-ItemProperty $Item.Path) -like "*$($Item.Name)*")
+    {
+        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+    else
+    {
+        New-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
+    }
+}
