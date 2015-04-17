3d630bd56998c2cbf8ce2ba66e6ed5a26178515d
MFGN <ignify@gmail.com>
2015-04-17 09:36:55 +0100

    Standardize reg tweaks

--- scripts/border-width.ps1
+++ scripts/border-width.ps1
@@ -1,33 +1,43 @@
-﻿if ((Get-PSDrive -PSProvider Registry).Root -notcontains 'HKEY_USERS')
+﻿$RegistryKeys = @(
+    [pscustomobject]@{
+        'Name' = "BorderWidth"
+        'Path' = "HKCU:\Control Panel\Desktop\WindowMetrics\"
+        'RegValue' = 0
+        'RegType' = 'String'
+    },
+    [pscustomobject]@{
+        'Name' = "PaddedBorderWidth"
+        'Path' = "HKCU:\Control Panel\Desktop\WindowMetrics\"
+        'RegValue' = 0
+        'RegType' = 'String'
+    },
+    [pscustomobject]@{
+        'Name' = "BorderWidth"
+        'Path' = "HKU:\.DEFAULT\Control Panel\Desktop\WindowMetrics\"
+        'RegValue' = 0
+        'RegType' = 'String'
+    },
+    [pscustomobject]@{
+        'Name' = "PaddedBorderWidth"
+        'Path' = "HKU:\.DEFAULT\Control Panel\Desktop\WindowMetrics\"
+        'RegValue' = 0
+        'RegType' = 'String'
+    }
+)
+
+if ((Get-PSDrive -PSProvider Registry).Root -notcontains 'HKEY_USERS')
 {
     New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
 }
 
-$Keys=@{
-    "BorderWidth" =  0
-    "PaddedBorderWidth" = 0
-}
-
-$HKCUpath = "HKCU:\Control Panel\Desktop\WindowMetrics\"
-$HKUpath = "HKU:\.DEFAULT\Control Panel\Desktop\WindowMetrics\"
-
-$Keys.GetEnumerator() | % {
-    #Change border width for current user
-    if ((Get-ItemProperty $HKCUpath) -like "*$($_.Name)*")
+ForEach ($Item in $RegistryKeys)
+{
+    if ((Get-ItemProperty $Item.Path) -like "*$($Item.Name)*")
     {
-        Set-ItemProperty -Path $HKCUpath -Name $_.Name -Value $_.Value -Type String -Force
+        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
     }
     else
     {
-        New-ItemProperty -Path $HKCUpath -Name $_.Name -Value $_.Value -Type String -Force 
-    }
-    #Change border width for all users
-    if ((Get-ItemProperty $HKUpath) -like "*$($_.Name)*")
-    {
-        Set-ItemProperty -Path $HKUpath -Name $_.Name -Value $_.Value -Type String -Force
-    }
-    else
-    {
-        New-ItemProperty -Path $HKUpath -Name $_.Name -Value $_.Value -Type String -Force
+        New-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.RegValue -Type $Item.RegType -Force
     }
 }
