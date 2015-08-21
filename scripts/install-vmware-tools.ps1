26ddcf798aa9bbb464eda40f51f0b4f968be8663
MFGN <ignify@gmail.com>
2015-08-21 15:47:17 +0100

    core builds future templates

---
+++ scripts/install-vmware-tools.ps1
@@ -0,0 +1,21 @@
+﻿$user = 'vagrant'
+$iso_path = "C:\Users\$($user)\windows.iso"
+
+if (Test-Path $iso_path) {
+    try {
+        $image_mount = Mount-DiskImage $iso_path -StorageType ISO -PassThru -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Error mounting ISO"
+        $error[0].Exception
+    }
+    Set-Location "$((Get-Volume -DiskImage $image_mount).DriveLetter):"
+    Start-Process -FilePath .\setup.exe -ArgumentList '/S /v "/qn REBOOT=ReallySuppress ADDLOCAL=ALL"' -Wait
+    if (!(Get-Service -Name VMTools -ErrorAction SilentlyContinue)) {
+        Write-Error "VMware Tools service not running"
+    }
+    Dismount-DiskImage -StorageType ISO -ImagePath $iso_path
+    Remove-Item -Path $iso_path -Force
+    Sleep-Sleep -Seconds 5
+    Restart-Computer -Force
+}
