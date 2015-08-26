ad9b2b283ef2f05f387863751b6e9ebcac1ee385
MFGN <ignify@gmail.com>
2015-08-26 14:33:15 +0100

    build working

--- scripts/install-vmware-tools-test.ps1
+++ scripts/install-vmware-tools-test.ps1
@@ -1,5 +1,5 @@
 ﻿param (
-    [string] $user = $env:username
+    [string]$user = $env:username
 )
 
 $iso_path = "C:\Users\$($user)\windows.iso"
@@ -15,24 +15,22 @@ if (Test-Path $iso_path) {
     }
     if ($image_mount) {
         $drive_letter = ((Get-Volume -DiskImage $image_mount).DriveLetter)+':'
-        Push-Location $drive_letter
+        $inst_path = Join-Path $drive_letter -ChildPath 'setup64.exe'
+
+
+        Start-Job -ScriptBlock {Start-Process -FilePath $inst_path -ArgumentList '/S /v "/qn REBOOT=R ADDLOCAL=ALL"' -Wait} | Get-Job | Wait-Job -TimeoutSec 180
+
+
         Write-Host "VMware Tools: Start installation - $(Get-Date -Format T)"
-        $installation_process = Start-Process -FilePath $($drive_letter+"\setup.exe") -ArgumentList '/S /v "/qn REBOOT=R ADDLOCAL=ALL"' -NoNewWindow -Wait -PassThru
-        Wait-Process -InputObject $installation_process
-        #$installation_args = "/c $($drive_letter)\setup.exe /S /v ""/qn REBOOT=R ADDLOCAL=ALL"""
-        #$installation_process = Start-Process 'cmd.exe' -ArgumentList $installation_args -PassThru -Wait
 
         Write-Host "VMware Tools: End installation - $(Get-Date -Format T)"
-        Pop-Location
         if (!(Get-Service -Name VMTools -ErrorAction SilentlyContinue)) {
             Write-Host "VMware Tools: Service not running" -ForegroundColor Red
         }
         else {
             Write-Host "VMware Tools: Service is running" -ForegroundColor Green
+            Write-Host "VMware Tools: Dismounting ISO"
+            Dismount-DiskImage -StorageType ISO -ImagePath $iso_path
         }
-        Write-Host "VMware Tools: Dismounting ISO"
-        Dismount-DiskImage -StorageType ISO -ImagePath $iso_path
-        #Write-Host "VMware Tools | Removing ISO"
-        #Remove-Item -Path $iso_path -Force
     }
 }
