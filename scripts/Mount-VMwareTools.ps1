7ca83e1e49df4853699711795a0ffd9286566013
MFGN <ignify@gmail.com>
2015-08-26 15:58:41 +0100

    reorganizing directory structure

---
+++ scripts/Mount-VMwareTools.ps1
@@ -0,0 +1,26 @@
+﻿param (
+    [string]$user = $env:username
+)
+
+$iso_path = "C:\Users\$($user)\windows.iso"
+
+if (Test-Path $iso_path) {
+    try {
+        Write-Host "VMware Tools: Mounting ISO"
+        $image_mount = Mount-DiskImage $iso_path -PassThru -ErrorAction Stop
+    }
+    catch {
+        Write-Host "VMware Tools: Error mounting ISO" -ForegroundColor Red
+        $error[0].Exception
+    }
+    if ($image_mount) {
+        $drive_letter = ((Get-Volume -DiskImage $image_mount).DriveLetter)+':'
+        $inst_path = Join-Path $drive_letter -ChildPath 'setup64.exe'
+        if (Test-Path $inst_path) {
+            Write-Host "VMware Tools: Start installation on $inst_path"
+        }
+        else {
+            Write-Host "VMware Tools: Error accessing $inst_path"
+        }
+    }
+}
