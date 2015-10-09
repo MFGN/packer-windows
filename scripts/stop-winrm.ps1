3cc0aca7ca3123d653e21aab555fe875df4e08f4
MFGN <ignify@gmail.com>
2015-10-09 16:36:34 +0100

    core full build complete

---
+++ scripts/stop-winrm.ps1
@@ -0,0 +1,5 @@
+﻿if ((Get-Service -Name WinRM).Status -eq 'Running') {
+    Stop-Service -Name WinRM -Force -PassThru
+}
+
+Set-Service -Name WinRM -StartupType Disabled -PassThru
--- scripts/winrm-tools.ps1
+++ scripts/winrm-tools.ps1
@@ -7,23 +7,18 @@ if (($id -eq $null) -and (Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Window
     New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -name LocalAccountTokenFilterPolicy -value 1 -propertyType dword
 }
 
-if ((Get-Service -Name WinRM).Status -notlike "Stopped") {
-    &net stop winrm
-    Enable-PSRemoting -Force
+Set-Service -Name WinRM -StartupType Automatic -PassThru
+
+Set-Item -Path WSMan:\localhost\Client\Auth\Basic -Value $true -Force -PassThru
+Set-Item -Path WSMan:\localhost\Client\AllowUnencrypted -Value $true -Force -PassThru
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true -Force -PassThru
+Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true -Force -PassThru
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "*" -Force -PassThru
+Set-Item -Path WSMan:\localhost\MaxTimeoutms -Value 1800000 -Force -PassThru
+Set-Item -Path WSMan:\localhost\Shell\MaxMemoryPerShellMB -Value 512 -Force -PassThru
+
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+
+if ((Get-Service -Name WinRM).Status -ne 'Running') {
+    Start-Service -Name WinRM -PassThru 
 }
-
-
-Enable-PSRemoting -Force
-
-
-#Set-Service -Name WinRM -StartupType Automatic
-
-# Enable and configure WinRM (see below)
-&winrm quickconfig `-q
-&winrm set winrm/config/client/auth '@{Basic="true"}'
-&winrm set winrm/config/service/auth '@{Basic="true"}'
-&winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="512"}'
-&winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-&winrm set winrm/config/client '@{AllowUnencrypted="true"}'
-&winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-&netsh http delete iplisten ipaddress=127.0.0.1
